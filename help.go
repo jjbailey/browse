e0acfe865ffaef4b8ed639b549eb2a9e363f10c5
Jack Bailey <jack@internetguy.net>
2024-02-16 13:02:54 -0800

    bump version number

--- help.go
+++ help.go
@@ -16,7 +16,7 @@ func (x *browseObj) printHelp() {
 
 	lines := []string{
 		"                                                               ",
-		"   Browse                       Version 0.18                   ",
+		"   Browse                       Version 0.19                   ",
 		"                                                               ",
 		"   Command                      Function                       ",
 		"   f b [PAGE UP] [PAGE DOWN]    Page down/up                   ",
