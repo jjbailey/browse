d22263aaaad6c0557ef1b535c6ea3a20e4355fb5
Jack Bailey <jack@internetguy.net>
2025-03-09 10:55:44 -0700

    partially revert 22fa9c3a3e61165c8d4fd005f9bcf59ecb58e0d3

--- scroll.go
+++ scroll.go
@@ -64,6 +64,11 @@ func (br *browseObj) scrollUp(count int) {
 
 	rowsToScroll := minimum(count, br.firstRow)
 
+	if br.shownEOF {
+		// cursor is on the bottom line
+		moveCursor(2, 1, false)
+	}
+
 	for i := 0; i < rowsToScroll; i++ {
 		br.firstRow--
 		br.lastRow--
