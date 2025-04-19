2b65574cd26738736d7d805dc082a75e3bdd3fad
Jack Bailey <jack@internetguy.net>
2025-04-19 13:58:03 -0700

    v0.50 broke readStdin (#48)

--- readfile.go
+++ readfile.go
@@ -20,44 +20,17 @@ import (
 )
 
 var (
-	mutex sync.RWMutex
+	mutex sync.Mutex
 )
-
-// FileReader handles the file reading operations
-type FileReader struct {
-	reader    *bufio.Reader
-	file      *os.File
-	notified  bool
-	stopChan  chan struct{}
-	errorChan chan error
-}
-
-// NewFileReader creates a new FileReader instance
-func NewFileReader(fp *os.File) *FileReader {
-	return &FileReader{
-		reader:    bufio.NewReader(fp),
-		file:      fp,
-		stopChan:  make(chan struct{}),
-		errorChan: make(chan error, 1),
-	}
-}
-
-func (fr *FileReader) Stop() {
-	// signals the reader to stop
-	close(fr.stopChan)
-}
 
 func readFile(br *browseObj, ch chan bool) {
 	// initial read file, and continuously read for updates
 
 	var bytesRead int64
+	var notified bool
 	var err error
 
-	fr := NewFileReader(br.fp)
-	defer func() {
-		fr.Stop()
-		close(ch)
-	}()
+	reader := bufio.NewReader(br.fp)
 
 	if br.fromStdin {
 		// wait for some input from stdin
@@ -67,98 +40,59 @@ func readFile(br *browseObj, ch chan bool) {
 	br.newFileSiz, br.savFileSiz = 0, 0
 
 	for {
-		select {
-
-		case <-fr.stopChan:
-			return
-
-		default:
-			// get file size with minimal mutex lock
-			mutex.RLock()
-			br.newFileSiz, err = getFileSize(br.fp)
-			mutex.RUnlock()
-
-			if err != nil {
-				if !fr.notified {
-					ch <- false
-					fr.errorChan <- err
-				}
-
-				return
-			}
-
-			// handle file truncation
-			if br.newFileSiz < br.savFileSiz {
-				mutex.Lock()
-
-				if err := br.handleFileTruncation(); err != nil {
-					mutex.Unlock()
-					fr.errorChan <- err
-					return
-				}
-
-				bytesRead = 0
-				mutex.Unlock()
-			}
-
-			// read new content if file grew
-			if br.savFileSiz == 0 || br.savFileSiz < br.newFileSiz {
-				if err := br.readNewContent(fr, &bytesRead); err != nil {
-					fr.errorChan <- err
-					return
-				}
-
-				if !fr.notified {
-					ch <- true
-					fr.notified = true
-				}
-			}
-
-			time.Sleep(500 * time.Millisecond)
-		}
-	}
-}
-
-func (br *browseObj) handleFileTruncation() error {
-	br.fileInit(br.fp, br.fileName, br.title, br.fromStdin)
-	br.printMessage("File truncated", MSG_RED)
-	br.modeScroll = MODE_SCROLL_NONE
-	br.shownMsg = false
-	br.savFileSiz = 0
-	return nil
-}
-
-func (br *browseObj) readNewContent(fr *FileReader, bytesRead *int64) error {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	_, err := br.fp.Seek(br.seekMap[br.mapSiz], io.SeekStart)
-
-	if err != nil {
-		return err
-	}
-
-	for {
-		br.seekMap[br.mapSiz] = *bytesRead
-		line, err := fr.reader.ReadString('\n')
-
+		// Get file size with minimal mutex lock
+		mutex.Lock()
+		br.newFileSiz, err = getFileSize(br.fp)
 		if err != nil {
-			if err != io.EOF {
-				return err
+			if !notified {
+				ch <- false
 			}
+			mutex.Unlock()
+			return
+		}
+		mutex.Unlock()
 
-			break
+		// Handle file truncation
+		if br.newFileSiz < br.savFileSiz {
+			mutex.Lock()
+			br.fileInit(br.fp, br.fileName, br.title, br.fromStdin)
+			br.printMessage("File truncated", MSG_RED)
+			br.modeScroll = MODE_SCROLL_NONE
+			br.shownMsg = false
+			br.savFileSiz, bytesRead = 0, 0
+			mutex.Unlock()
 		}
 
-		lineLen := len(line)
-		*bytesRead += int64(lineLen)
-		br.sizeMap[br.mapSiz] = int64(minimum(lineLen-1, READBUFSIZ))
-		br.mapSiz++
-	}
+		// Read new content if file grew
+		if br.savFileSiz == 0 || br.savFileSiz < br.newFileSiz {
+			mutex.Lock()
+			br.fp.Seek(br.seekMap[br.mapSiz], io.SeekStart)
 
-	br.hitEOF = false
-	br.savFileSiz = br.newFileSiz
-	return nil
+			for {
+				br.seekMap[br.mapSiz] = bytesRead
+				line, err := reader.ReadString('\n')
+				if err != nil {
+					break
+				}
+
+				lineLen := len(line)
+				bytesRead += int64(lineLen)
+				br.sizeMap[br.mapSiz] = int64(minimum(lineLen-1, READBUFSIZ))
+				br.mapSiz++
+			}
+
+			br.hitEOF = false
+			br.savFileSiz = br.newFileSiz
+
+			if !notified {
+				ch <- true
+				notified = true
+			}
+			mutex.Unlock()
+		}
+
+		time.Sleep(time.Second)
+	}
 }
 
 func getFileSize(fp *os.File) (int64, error) {
@@ -182,17 +116,22 @@ func (br *browseObj) readStdin(fin, fout *os.File) error {
 		line, err := r.ReadString('\n')
 
 		if err != nil {
-			if errors.Is(err, io.EOF) {
+			if errors.Is(err, syscall.EPIPE) {
+				br.saneExit()
+			}
+
+			if err == io.EOF {
 				break
 			}
 
-			if errors.Is(err, syscall.EPIPE) ||
-				errors.Is(err, syscall.EINVAL) || errors.Is(err, io.ErrClosedPipe) {
-				br.saneExit()
-			}
+			return err
 		}
 
 		if _, err := w.WriteString(line); err != nil {
+			return err
+		}
+
+		if err := w.Flush(); err != nil {
 			return err
 		}
 	}
@@ -203,22 +142,18 @@ func (br *browseObj) readStdin(fin, fout *os.File) error {
 func (br *browseObj) readFromMap(lineno int) []byte {
 	// use the maps to read a line from the file
 
-	mutex.RLock()
-	defer mutex.RUnlock()
+	mutex.Lock()
+	defer mutex.Unlock()
 
 	if lineno >= br.mapSiz {
+		// should not happen
 		return nil
 	}
 
-	size := br.sizeMap[lineno]
-	if size <= 0 || size > READBUFSIZ {
-		return nil
-	}
+	data := make([]byte, br.sizeMap[lineno])
+	_, err := br.fp.ReadAt(data, br.seekMap[lineno])
 
-	data := make([]byte, size)
-	n, err := br.fp.ReadAt(data, br.seekMap[lineno])
-
-	if err != nil || n == 0 {
+	if err != nil || len(data) == 0 {
 		return nil
 	}
 
