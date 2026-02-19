9846ae1d4e74dc5622779d2748515b54d8463877
Jack Bailey <jack@internetguy.net>
2026-02-18 17:35:33 -0800

    bump version number

--- extern.go
+++ extern.go
@@ -19,7 +19,7 @@ import (
 
 // BR_VERSION is the current application version.
 const (
-	BR_VERSION = "1.0.0"
+	BR_VERSION = "1.0.1"
 )
 
 // ─── Constants ──────────────────────────────────────────────────────
