c86d0a859b17d95434b9fe265a96458af7cb64eb
Jack Bailey <jack@internetguy.net>
2026-07-01 18:29:08 -0700

    handle sigchld separately

--- signals.go
+++ signals.go
@@ -57,7 +57,8 @@ func (br *browseObj) catchSignals() {
 
 	sigChan = make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGWINCH)
-	signal.Ignore(syscall.SIGALRM, syscall.SIGCHLD, syscall.SIGURG)
+	signal.Reset(syscall.SIGCHLD)
+	signal.Ignore(syscall.SIGALRM, syscall.SIGURG)
 
 	go func() {
 		for sig := range sigChan {
